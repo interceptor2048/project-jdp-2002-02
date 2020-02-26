46946639d1b27b36c2705f3ba34ffe8b9d314992
g-chmura <gchmura98@gmail.com>
2020-02-26 09:26:03 +0100

    Initial ProductController implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -8,29 +8,29 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("v1/ecommercee")
+@RequestMapping("api/v1/ecommercee")
 @Transactional
 public class ProductController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getAllProducts", consumes = "application/json")
-    public List<ProductDto> getAllProducts(){
+    @GetMapping(value = "products", consumes = "application/json")
+    public List<ProductDto> getAllProducts() {
         return new ArrayList<ProductDto>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
-    public ProductDto getProduct(@RequestParam Long productId){
+    @PostMapping(value = "products", consumes = "application/json")
+    public ProductDto getProduct(@RequestParam Long productId) {
         return new ProductDto() ;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "newProduct", consumes = "application/json")
-    public void createProduct(@RequestBody ProductDto productDto){
+    @PutMapping(value = "products", consumes = "application/json")
+    public void createProduct(@RequestBody ProductDto productDto) {
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct", consumes = "application/json")
+    @PutMapping(value = "products", consumes = "application/json")
     public void updateProduct(@RequestBody ProductDto productDto) {
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct", consumes = "application/json")
+    @DeleteMapping(value = "products", consumes = "application/json")
     public void deleteProduct(@RequestParam ProductDto productDto) {
     }
 }
