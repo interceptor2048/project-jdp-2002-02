9056626fcde7701ec63ffd0ad694cff6295bc18e
g-chmura <gchmura98@gmail.com>
2020-02-27 09:48:45 +0100

    Cart Entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "carts")
 public class Cart {
+
+    @GeneratedValue
+    @Id
+    @NotNull
+    @Column(name = "id")
+    private long id;
+
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_name")
+    private User user;
+
+    @OneToMany(mappedBy = "cartId", fetch = FetchType.EAGER)
+    private List<Product> products;
+
+    public Cart(User user) {
+        this.user = user;
+        this.products = new ArrayList<>();
+    }
 }
