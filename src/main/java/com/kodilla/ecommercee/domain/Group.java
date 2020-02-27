3f4d73424cdf3775cb66ec0e8f6310de43679a88
Gabriela Chmura <54775821+g-chmura@users.noreply.github.com>
2020-02-27 12:45:20 +0100

    Group Entity

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,4 +1,31 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups")
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "groupId")
+    private List<Product> products = new ArrayList<>();
+
+    public Group(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
