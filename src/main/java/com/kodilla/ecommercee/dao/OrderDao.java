6f5cf19325d23badedc4e5bb191aa3c59e79d4b8
Emilia <emicoder@interia.pl>
2020-02-28 11:12:32 +0100

    updated

--- src/main/java/com/kodilla/ecommercee/dao/OrderDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/OrderDao.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface OrderDao extends CrudRepository<Order, Long> {
-    List<Order> findByStatus(String status);
+
 }
