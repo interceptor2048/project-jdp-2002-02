a19f1ca185b44012831744f1302af80db33836e3
mateusz58 <matp321@gmail.com>
2020-02-28 09:22:47 +0100

    Corrected minor errors

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,35 +1,26 @@
 package com.kodilla.ecommercee.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Entity
 @Table(name="users")
 public class User {
 
     @Id
-    @Column(name =id)
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     @Column(name="first_name")
-    @NotNull
-    private String first_name;
+    private String firstName;
 
     @Column(name="last_name")
-    @NotNull
-    private String last_name;
+    private String lastName;
 
     @OneToMany(
             targetEntity=Order.class,
