d864147ec66b84263ac4f56b307dfa343d0f807e
g-chmura <gchmura98@gmail.com>
2020-02-28 12:44:31 +0100

    GroupController extended implementation

--- src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import com.kodilla.ecommercee.domain.Product;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class GroupDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private List<Product> products;
+
+    public GroupDto(Long id, String name, String description, List<Product> products) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.products = products;
+    }
+
+    public GroupDto() {
+    }
 }
