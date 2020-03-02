a5562d253dbb52f4693732ba76ce1df47e525e26
Emilia <emicoder@interia.pl>
2020-03-02 11:31:52 +0100

    updated

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -1,31 +1,4 @@
 package com.kodilla.ecommercee.domain.dto;
 
 public class ProductDto {
-    private Long id;
-    private String name;
-
-    public ProductDto() {
-
-    }
-
-    public ProductDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
