e488074f366335e52fe50bb29fe3b95723f0bd17
Emilia <emicoder@interia.pl>
2020-03-03 10:17:41 +0100

    Encja Group Testy updated1

--- src/main/java/com/kodilla/ecommercee/dao/UserDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.dao;
 
-import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDao extends CrudRepository<User, Long> {
