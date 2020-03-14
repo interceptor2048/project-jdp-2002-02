9b66ce29854a360300d8ff6e58785c56f8d470ed
Emilia <emicoder@interia.pl>
2020-03-14 19:10:37 +0100

    14.03.

--- src/main/java/com/kodilla/ecommercee/dao/CartDao.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.ecommercee.dao;
-
-import com.kodilla.ecommercee.domain.Cart;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CartDao extends CrudRepository<Cart, Long> {
-}
--- src/main/java/com/kodilla/ecommercee/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.ecommercee.dao;
-
-import com.kodilla.ecommercee.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDao extends CrudRepository<User, Long> {
-}
