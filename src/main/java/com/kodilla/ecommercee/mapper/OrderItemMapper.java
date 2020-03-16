f5671803a28a5df10b93bc32c624fce61ea86852
kkazm <s14005@pjwstk.edu.pl>
2020-03-16 22:28:41 +0100

    add mapToOrderItemDtoList() to mapper

--- src/main/java/com/kodilla/ecommercee/mapper/OrderItemMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderItemMapper.java
@@ -7,6 +7,9 @@ import com.kodilla.ecommercee.domain.dto.OrderItemDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class OrderItemMapper {
    @Autowired
@@ -31,5 +34,16 @@ public class OrderItemMapper {
                 orderItem.getProduct().getId(),
                 orderItem.getCart().getId()
         );
+    }
+
+    public List<OrderItemDto> mapToOrderItemDtoList(final List<OrderItem> orderItemList) {
+        return orderItemList.stream()
+                .map(o -> new OrderItemDto(
+                        o.getId(),
+                        o.getQuantity(),
+                        o.getProduct().getId(),
+                        o.getCart().getId()
+                ))
+                .collect(Collectors.toList());
     }
 }
