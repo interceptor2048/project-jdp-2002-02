a6788cb21f957f87e645c84b595fcf510eabdbff
TomaszTomasiak <55875232+TomaszTomasiak@users.noreply.github.com>
2020-03-16 19:29:59 +0100

    Change Request
    trzeba jeszcze dodać:
    Product findProductById(Long id) w ProductDao i
    Cart findCartById(Long id) w CartDao

--- src/main/java/com/kodilla/ecommercee/mapper/OrderItemMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderItemMapper.java
@@ -6,21 +6,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderItemMapper {
+   @Autowired
+    ProductDao productRepository;
+
+    @Autowired
+    CartDao cartRepository;
+
     public OrderItem mapToOrderItem(final OrderItemDto orderItemDto){
         return new OrderItem(
                 orderItemDto.getId(),
-                orderItemDto.getQuantity(),
-                orderItemDto.getProduct(),
-                orderItemDto.getCart()
+                cartRepository.findCartById(orderItemDto.getCart()),
+                productRepository.findProductById(orderItemDto.getProductId()),
+                orderItemDto.getQuantity()
         );
     }
 
     public OrderItemDto mapToOrderItemDto(final OrderItem orderItem){
         return new OrderItemDto(
                 orderItem.getId(),
-                orderItem.getQuantity(),
-                orderItem.getProduct(),
-                orderItem.getCart()
+                orderItem.getCart().getId(),
+                orderItem.getProduct().getId(),
+                orderItem.getQuantity()
         );
+    }
+
+    public List<OrderItemDto> mapToOrderItemDtoList(final List<OrderItem> orderItemList) {
+        return orderItemList.stream()
+                .map(o -> new OrderItemDto(o.getId(), o.getCart().getId(), o.getProduct().getId(), o.getQuantity()))
+                .collect(Collectors.toList());
     }
 }
