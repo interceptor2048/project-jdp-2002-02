7b8d954cdf90d777f8b7fcde2dde45ee640e8f9b
mateusz58 <matp321@gmail.com>
2020-03-17 18:32:45 +0100

    Master

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
